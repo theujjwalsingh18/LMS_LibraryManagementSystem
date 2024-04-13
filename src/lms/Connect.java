f62c48e2c9beee0d66b45e75d7be8e2c3f17e67e
Ujjwal Singh <theujjwalsinghh@gmail.com>
2024-04-13 13:19:07 +0530

    Added

---
+++ src/lms/Connect.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ujjwal
+ */
+public class Connect {
+    public static Connection Connection() {
+        Connection con=null;
+        try {
+             con=DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","admin");
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+}
+}
